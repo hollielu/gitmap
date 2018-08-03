f2c9b03f9c1fed80c17ad84acf0fa25ebbc46d04
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-03 10:16:56 -0500

    Clean up file structure

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,59 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
-import {Menu, Button} from 'semantic-ui-react'
+import {Menu} from 'semantic-ui-react'
 
-export const Navbar = ({handleClick, isLoggedIn}) => (
+const Navbar = () => (
   <Menu attached="top">
     <Menu.Item name="home" as={Link} to="/">
-      Home
+      GitMap
     </Menu.Item>
-    <Menu.Menu position="right">
-      {isLoggedIn ? (
-        <Menu.Item>
-          {/* The navbar will show these links after you log in */}
-          <Button onClick={handleClick}>Logout</Button>
-        </Menu.Item>
-      ) : (
-        <Menu.Item>
-          {/* The navbar will show these links before you log in */}
-          <Button as={Link} to="/signup" primary>
-            Sign Up
-          </Button>
-          <Button as={Link} to="/login">
-            Login
-          </Button>
-        </Menu.Item>
-      )}
-    </Menu.Menu>
   </Menu>
 )
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
-
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+export default Navbar
--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,7 @@
 const router = require('express').Router()
-const triggerSendEmail = require('./triggers/send-email')
 module.exports = router
 
 router.use('/users', require('./users'))
-
-router.get('/test', (req, res, next) => {
-  triggerSendEmail('test_event', 'please', 'work', 'hello')
-  res.json({test: 'test'})
-})
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
