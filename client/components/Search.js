7df54313e3fc576d04ea912e322b4515cf5f0eac
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-03 17:18:27 -0500

    set up user/repo search

---
+++ client/components/Search.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getContributorsLocations} from '../store/'
+import {Input, Button} from 'semantic-ui-react'
+
+const mapDispatch = dispatch => ({
+  getContributorsLocations: data => dispatch(getContributorsLocations(data))
+})
+
+class Search extends Component {
+  constructor() {
+    super()
+    this.state = {
+      owner: '',
+      repo: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit = () => {
+    const {getContributorsLocations} = this.props
+    const {owner, repo} = this.state
+    getContributorsLocations({owner, repo})
+    this.setState({repo: '', owner: ''})
+  }
+
+  render() {
+    return (
+      <div>
+        <Input
+          name="owner"
+          placeholder="Search users..."
+          onChange={this.handleChange}
+        />
+        <Input
+          name="repo"
+          placeholder="Search repos..."
+          onChange={this.handleChange}
+          style={{paddingRight: 10}}
+        />
+        <Button onClick={this.handleSubmit}>Search</Button>
+      </div>
+    )
+  }
+}
+
+export default connect(null, mapDispatch)(Search)
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {Menu, Button} from 'semantic-ui-react'
+import {Menu, Header, Icon} from 'semantic-ui-react'
+import {Search} from '../components'
 
 const Navbar = () => (
-  <Menu attached="top">
-    <Button name="home" as={Link} to="/">
-      GitMap
-    </Button>
+  <Menu pointing secondary>
+    <Menu.Item style={styles.menu}>
+      <Header>
+        <Icon name="map marker alternate" /> GitMap
+      </Header>
+    </Menu.Item>
+    <Menu.Menu position="right" style={styles.button}>
+      <Search />
+    </Menu.Menu>
   </Menu>
 )
+
+const styles = {
+  menu: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  button: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 20
+  }
+}
 
 export default Navbar
