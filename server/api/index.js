9ab0d74b2b4ea7e26f8bf90976ef84cfe0d19bb9
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-03 12:51:08 -0500

    /repositories routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,21 @@
 const router = require('express').Router()
+const axios = require('axios')
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/repos', require('./repos'))
+router.use('/codes_of_conduct', require('./codes_of_conduct'))
+
+// every public repository, in the order that they were created
+router.get('/repositories', async (req, res, next) => {
+  try {
+    const url = `https://api.github.com/repositories`
+    const {data} = await axios.get(url)
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
