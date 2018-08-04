5aa552751b7770e391c9f4c97927ae927d3845ab
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-04 13:15:50 -0500

    remove map

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import {Container, Segment} from 'semantic-ui-react'
-import {Map, DataMap} from '../components'
 
 const Home = () => {
   return (
     <Container style={{marginTop: 10, marginBottom: 10}}>
-      <Segment>
-        <Map />
-      </Segment>
+      <Segment />
     </Container>
   )
 }
