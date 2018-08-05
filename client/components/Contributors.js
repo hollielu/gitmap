c7bd21ab9711d846ca56ec22df87e428819fe35c
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-05 17:55:01 -0500

    add contributors to repo

--- client/components/Contributors.js
+++ client/components/Contributors.js
@@ -1,18 +1,49 @@
-import React from 'react'
-import {Segment} from 'semantic-ui-react'
+import React, {Component} from 'react'
+import axios from 'axios'
+import {Segment, Header, Image} from 'semantic-ui-react'
 
-const Contributors = () => {
-  return (
-    <Segment padded floated="left" style={{fontSize: '20px'}}>
-      Click on a{' '}
-      <img
-        src="https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"
-        height="40"
-        width="40"
-      />{' '}
-      on the map to zoom in on a location.
-    </Segment>
-  )
+class Contributors extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentRepo: {}
+    }
+  }
+
+  async componentDidMount() {
+    const {owner, repo} = this.props
+    const {data} = await axios.get(`/api/repos/${owner}/${repo}`)
+    this.setState({currentRepo: data})
+  }
+
+  async componentDidUpdate(prevProps) {
+    const {owner, repo} = this.props
+    if (owner !== prevProps.owner || repo !== prevProps.repo) {
+      const {data} = await axios.get(`/api/repos/${owner}/${repo}`)
+      this.setState({currentRepo: data})
+    }
+  }
+
+  render() {
+    const {currentRepo} = this.state
+    const {name, owner, avatarUrl, ownerUrl, repoUrl, description} = currentRepo
+    return (
+      <Segment padded>
+        <Header as="h1" block>
+          <a href={repoUrl}>{name}</a> by <a href={ownerUrl}>{owner}</a>
+        </Header>
+        <Image src={avatarUrl} size="mini" floated="left" />
+        <p style={styles.font}>{description}</p>
+      </Segment>
+    )
+  }
+}
+
+const styles = {
+  font: {
+    fontFamily: 'Do Hyeon',
+    fontSize: '20px'
+  }
 }
 
 export default Contributors
