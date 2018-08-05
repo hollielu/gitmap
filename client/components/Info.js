b83349f48bb4fc09f75a8c5fba6003db4703ba07
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-05 01:58:28 -0500

    info about app

---
+++ client/components/Info.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Segment} from 'semantic-ui-react'
+
+const Info = () => {
+  return (
+    <Segment padded floated="left" style={{fontSize: '20px'}}>
+      Welcome to GitMap! 27 million software developers currently collaborate on{' '}
+      <a href="https://github.com/">Github</a> across 200 countries.<a href="https://techcrunch.com/2018/06/03/microsoft-is-reportedly-acquiring-github/">
+        *
+      </a>{' '}
+      GitMap plots the locations of contributors to any open-source project on a
+      map, so you can see all the people that came together to make amazing
+      things happen.
+    </Segment>
+  )
+}
+
+export default Info
