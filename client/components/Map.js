66b4709de9f6dbd601239c61663af364381c237e
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-04 21:00:25 -0500

    map zoom working

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import MapGL, {Marker, NavigationControl} from 'react-map-gl'
-import {Icon} from 'semantic-ui-react'
 
 const mapBoxStyle = 'mapbox://styles/hollielu/cjkg03sys1azg2spljgr104u5'
 
@@ -8,37 +7,54 @@ class Map extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      viewport: {
-        latitude: 0,
-        longitude: 0,
-        zoom: 0,
-        bearing: 0,
-        pitch: 0,
-        width: 1100,
-        height: 500
-      }
+      latitude: 0,
+      longitude: 0,
+      zoom: 0,
+      bearing: 0,
+      pitch: 0,
+      width: 1100,
+      height: 500
     }
+    this.renderMarkers = this.renderMarkers.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick = (latitude, longitude) => {
+    this.setState({latitude, longitude, zoom: 5})
   }
 
   renderMarkers = () => {
     const {coordinates} = this.props
     return coordinates.map((coordinate, idx) => {
       return (
-        <Marker key={idx} latitude={coordinate[1]} longitude={coordinate[2]}>
-          <Icon name="map pin" />
-        </Marker>
+        <div key={idx}>
+          <Marker latitude={coordinate[1]} longitude={coordinate[2]}>
+            <img
+              src="https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"
+              height="50"
+              width="50"
+              onClick={() => this.handleClick(coordinate[1], coordinate[2])}
+            />
+          </Marker>
+        </div>
       )
     })
   }
 
   render() {
-    const {viewport} = this.state
     return (
-      <MapGL {...viewport} mapStyle={mapBoxStyle} mapboxApiAccessToken="">
-        {this.renderMarkers()}
-        <div className="nav" style={navStyle}>
-          <NavigationControl />
+      <MapGL
+        {...this.state}
+        mapStyle={mapBoxStyle}
+        mapboxApiAccessToken="pk.eyJ1IjoiaG9sbGllbHUiLCJhIjoiY2prZnFyamlmMGM0ZTN4bXlhdnFoaGt3YiJ9.I8wRnROtntC4znYg15Td5g"
+        onViewportChange={viewport => this.setState({...viewport})}
+      >
+        <div style={{position: 'absolute', right: 0}}>
+          <NavigationControl
+            onViewportChange={viewport => this.setState({...viewport})}
+          />
         </div>
+        {this.renderMarkers()}
       </MapGL>
     )
   }
