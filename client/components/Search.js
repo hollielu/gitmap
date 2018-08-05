f0e09446fbf0a7caa8794a8f6562f8b296237483
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-05 01:58:57 -0500

    style search component

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,25 +1,52 @@
 import React from 'react'
-import {Input, Button} from 'semantic-ui-react'
+import {Input, Button, Segment} from 'semantic-ui-react'
 
 const Search = ({handleChange, handleSubmit, owner, repo}) => {
   return (
-    <div>
-      <Input
-        name="owner"
-        placeholder="Search users..."
-        onChange={handleChange}
-        value={owner}
-      />
-      <Input
-        name="repo"
-        placeholder="Search repos..."
-        onChange={handleChange}
-        value={repo}
-        style={{paddingRight: 10}}
-      />
-      <Button onClick={handleSubmit}>Search</Button>
-    </div>
+    <Segment padded floated="right">
+      <span style={styles.font}>
+        Enter the name of a GitHub repository and the owner to generate a map of
+        the locations of its contributors.
+      </span>
+      <br />
+      <br />
+      <div>
+        <Input
+          name="owner"
+          placeholder="Search owners..."
+          onChange={handleChange}
+          value={owner}
+          style={styles.input}
+        />
+        <Input
+          name="repo"
+          placeholder="Search repos..."
+          onChange={handleChange}
+          value={repo}
+          style={{paddingRight: 10}}
+          style={styles.input}
+        />
+        <Button style={styles.button} onClick={handleSubmit}>
+          Map It
+        </Button>
+        <br />
+      </div>
+    </Segment>
   )
+}
+
+const styles = {
+  font: {
+    fontSize: '20px'
+  },
+  input: {
+    fontSize: '14px'
+  },
+  button: {
+    fontFamily: 'Do Hyeon',
+    fontSize: '15px',
+    marginLeft: 15
+  }
 }
 
 export default Search
