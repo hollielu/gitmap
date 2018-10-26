6a45dcc0667f384eda856911831b9e152a76af59
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-10-25 21:52:46 -0500

    remove secrets file

--- server/index.js
+++ server/index.js
@@ -1,63 +1,61 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const PORT = process.env.PORT || 8080
-const app = express()
-module.exports = app
-
-if (process.env.NODE_ENV !== 'production') require('../secrets')
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 8080;
+const app = express();
+module.exports = app;
 
 const createApp = () => {
   // logging middleware
-  app.use(morgan('dev'))
+  app.use(morgan("dev"));
 
   // body parsing middleware
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   // auth and api routes
-  app.use('/api', require('./api'))
+  app.use("/api", require("./api"));
 
   // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
+  app.use(express.static(path.join(__dirname, "..", "public")));
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
     if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
+      const err = new Error("Not found");
+      err.status = 404;
+      next(err);
     } else {
-      next()
+      next();
     }
-  })
+  });
 
   // sends index.html
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-  })
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public/index.html"));
+  });
 
   // error handling endware
   app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
-}
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
+};
 
 const startListening = () => {
   const server = app.listen(PORT, () =>
     console.log(`Mixing it up on port ${PORT}`)
-  )
-}
+  );
+};
 
 async function bootApp() {
-  await createApp()
-  await startListening()
+  await createApp();
+  await startListening();
 }
 
 if (require.main === module) {
-  bootApp()
+  bootApp();
 } else {
-  createApp()
+  createApp();
 }
