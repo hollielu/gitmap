02b7f8964b8308b6f57e12fd7b33db80b6306b2a
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-11-09 10:04:04 -0600

    update environment variables

--- server/api/index.js
+++ server/api/index.js
@@ -1,27 +1,27 @@
-const router = require('express').Router()
-const axios = require('axios')
-module.exports = router
+const router = require("express").Router();
+const axios = require("axios");
+module.exports = router;
 
-router.use('/users', require('./users'))
-router.use('/repos', require('./repos'))
+router.use("/users", require("./users"));
+router.use("/repos", require("./repos"));
 
 // oAuth tokens
-const id = process.env.GITHUB_CLIENT_ID
-const secret = process.env.GITHUB_CLIENT_SECRET
+const id = process.env.GITHUB_CLIENT_ID;
+const secret = process.env.GITHUB_CLIENT_SECRET;
 
 // every public repository, in the order that they were created
-router.get('/repositories', async (req, res, next) => {
+router.get("/repositories", async (req, res, next) => {
   try {
-    const url = `https://api.github.com/repositories?client_id=${id}&client_secret=${secret}`
-    const {data} = await axios.get(url)
-    res.json(data)
+    const url = `https://api.github.com/repositories?client_id=${id}&client_secret=${secret}`;
+    const { data } = await axios.get(url);
+    res.json(data);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
